ccb06ecc4bb84ee30c0d648af14dc574c7a9fb08
shafeeh011 <shafeeh011@.com>
2025-03-02 19:18:18 +0530

    azure deployment

--- app.py
+++ app.py
@@ -85,4 +85,5 @@ def live_predict():
 
 if __name__ == "__main__":
     clApp = ClientApp()
-    app.run(host='0.0.0.0', port=8080)
+    #app.run(host='0.0.0.0', port=8080) # AWS
+    app.run(host='0.0.0.0', port=80) # Azure
